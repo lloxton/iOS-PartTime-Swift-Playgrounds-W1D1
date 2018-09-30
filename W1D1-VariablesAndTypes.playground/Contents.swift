ef179a456c14edf477e758a1ed9f234efcfb34c8
Liam Loxton <liam.loxton@eventbase.com>
2018-09-30 17:56:40 -0400

    Answered Questions!

--- W1D1-VariablesAndTypes.playground/Contents.swift
+++ W1D1-VariablesAndTypes.playground/Contents.swift
@@ -33,18 +33,21 @@ count = 10
 //:
 //: Now it's your turn. Change `greeting` to be a different string by using the assignment operator.
 
-
+greeting = "Goodbye World"
 
 //: *Note:* unlike math, where `x = 10` and `10 = x` are interchangeable, in programming the variable goes on the left of the `=` symbol, and the new value goes on the right.
+
 
 
 //: ### Challenge 2
 //:
 //: We want to add 1 to whatever the value of `count` is now. We could manually look at the value in `count`, see that it is 10 and put 11 into `count` using the assignment operator. This works, but we can do it in a better way. We can set count to be whatever count is, plus 1. See if you can figure out the syntax for this.
 
+count = count+1
 
 //: *Note:* If you're ever unsure of the contents of a variable in a playground, write the variable on a line by itself and you will see its value printed to the right of it. Or you can use the `print` function like so: `print(variableName)`.
 
+print(count)
 
 //: ## Types
 //:
@@ -71,10 +74,12 @@ count
 var tipAmount = 6.01
 // tipAmount's type is:
 
+// Liam says 'double'
 
 var isOpen = true
 // isOpen's type is:
 
+// Liam says 'boolean'
 
 //: ## Type Inference
 //:
@@ -89,7 +94,7 @@ var height: Double = 10
 //: Since all our variables have types, even if we haven't specifically set them, we can't store any value in them... only values of the right type.
 //: Test this out. Try setting the value of `count` to be `height`:
 
-
+// count = height  #error
 
 //: It didn't work, right? They are both numbers, but `count` is an Int, and `height` is a `Double`. There are ways to convert the `Double` 10 into the `Int` 10, but we won't go into that just now.
 
@@ -104,6 +109,6 @@ var height: Double = 10
 
 var schoolName = "Lightouse Labs"
 
-
+greeting.lowercased()
 
 //: THE END
